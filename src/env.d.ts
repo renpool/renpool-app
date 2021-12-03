d9d07e585824f24c576f5731fd5c904a2ab81697
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-12-03 15:28:24 +0100

    Enhance vite env typings and docs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,69 @@
 /// <reference types="vite/client" />
 
+/*
+ * https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
+ */
+
 interface ImportMetaEnv {
-    readonly VITE_CHAIN_ID: keyof typeof import('$lib/config').NETWORKS;
+
+    /**
+     * The environment variable `VITE_CHAIN_ID` indicates which network
+     * this instance should connect to.
+     * For example, to connect to the _Kovan_ testnet, 
+     * use the following in your `.env` settings
+     * 
+     * ```
+     * VITE_CHAIN_ID=42
+     * ```
+     * 
+     * #### About its definition
+     * 
+     * Its type reflects which networks are supported by the application.
+     * The `NETWORKS` variable contains the settings for each supported network.
+     * With the `typeof` operator, we can its type.
+     * 
+     * ```ts
+     * type N = typeof NETWORKS;
+     * // type N = {
+     * //     1: {
+     * //         name: string; ...; contracts: { ...  };
+     * //     }; ...
+     * //     1337: { ... };
+     * // }
+     * ```
+     * 
+     * In turn, by using the `keyof` operator,
+     * we get a type representing only the its keys.
+     * 
+     * ```
+     * type K = keyof N;
+     * // type K = 1 | 42 | 1337
+     * ```
+     * 
+     * Notice that its keys are defined as `number`.
+     * But the value returned by `import.meta.env.VITE_CHAIN_ID` is a `string`.
+     * So we use a template literal type to have the string representation of
+     * a number. See
+     * https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html
+     * for more info.
+     * 
+     * ```
+     * type S = `${K}`;
+     * // type S = "1" | "42" | "1337"
+     * ```
+     * 
+     */
+    readonly VITE_CHAIN_ID: `${keyof typeof import('$lib/config').NETWORKS}`;
     readonly INFURA_PROJECT_ID: string;
 }
 
 interface ImportMeta {
     readonly env: ImportMetaEnv
+}
+
+/**
+ * https://github.com/MetaMask/providers
+ */
+interface Window {
+    ethereum: import('@metamask/providers').MetaMaskInpageProvider;
 }
