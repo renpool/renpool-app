42461e55ef81f845cd47658865698ee86498b927
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-12-05 21:16:57 +0100

    Set up vite mode

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
@@ -16,8 +16,10 @@ const config = {
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+
+		vite: {
+			mode: process.env.MODE || 'development',
+		},
 	},
 
 };
-
-export default config;
