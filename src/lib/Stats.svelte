4d599f8c1b98f504fc333df007aa8ecfc3e316aa
fede-rodes <federodes@gmail.com>
2021-12-08 15:17:04 +0100

    ref stats

--- src/lib/Stats.svelte
+++ src/lib/Stats.svelte
@@ -2,36 +2,26 @@
     export let totalPooled: string;
     export let isLocked: boolean;
     export let userBalance: string;
+
+    let rows: { title: string, value: any }[];
+
+    $: {
+      rows = [
+        { title: 'Total pooled', value: totalPooled },
+        { title: 'Pool is locked', value: isLocked },
+        { title: 'Your pool balance', value: userBalance },
+      ];
+    }
 </script>
 
 <section>
     <h2>Stats</h2>
     <div class="underline-title" />
 
-    <p>
-        <span class="key">Total Pooled</span>
-        <span class="value">{totalPooled}</span>
-    </p>
-    <p>
-        <span class="key">Pool is locked</span>
-        <span class="value">{isLocked}</span>
-    </p>
-    <p>
-        <span class="key">You pool balance</span>
-        <span class="value">{userBalance}</span>
-    </p>
+    {#each rows as { title, value }}
+      <p class="py-1">
+          <span class="font-medium">{title}</span>
+          <span class="pl-2 text-indigo-500">{value}</span>
+      </p>
+    {/each}
 </section>
-
-<style lang="postcss">
-    p {
-        @apply py-1;
-    }
-
-    p span.key {
-        @apply font-medium;
-    }
-
-    p span.value {
-        @apply pl-2 text-indigo-500;
-    }
-</style>
