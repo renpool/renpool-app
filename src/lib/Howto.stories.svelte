17a4babd374e0043d653e7ea7045d6431cd56783
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-12-09 14:33:10 +0100

    Showcase multiple Howtos in one page

--- src/lib/Howto.stories.svelte
+++ src/lib/Howto.stories.svelte
@@ -2,6 +2,12 @@
     import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
     import Howto from "./Howto.svelte";
     import Step from "./HowtoStep.svelte";
+
+    let steps = [];
+
+    function handleClick() {
+        steps = [...steps, "new step"];
+    }
 </script>
 
 <Meta title="$lib/Howto" component={Howto} argTypes={{}} />
@@ -28,6 +34,29 @@
             <div slot="desc">Something about what to do in this step #2.</div>
         </Step>
         <Step title="Should be Step #2">
+            <div slot="desc">Something about what to do in this step #3.</div>
+        </Step>
+    </Howto>
+</Story>
+
+<Story name="Multiple Howtos" args={{}}>
+    <button on:click={handleClick}>Add a new step, it keeps the right context</button>
+    <Howto title="Instructions #1">
+        <Step title="Should be Step #1.1">
+            <div slot="desc">Something about what to do in this step #1.</div>
+        </Step>
+        {#each steps as step}
+            <Step title={step} />
+        {/each}
+    </Howto>
+    <Howto title="Instructions #2">
+        <Step title="Should be Step #2.1">
+            <div slot="desc">Something about what to do in this step #1.</div>
+        </Step>
+        <Step title="Should be Step #2.2">
+            <div slot="desc">Something about what to do in this step #2.</div>
+        </Step>
+        <Step title="Should be Step #2.3">
             <div slot="desc">Something about what to do in this step #3.</div>
         </Step>
     </Howto>
